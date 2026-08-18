3df54b7272568a0e4384561d5c6f7b749e0b2eae
headdrop <ehdgmltn@gmail.com>
2026-08-18 13:55:08 +0900

    Feature: 채팅용 Agit 방 참조 도메인 추가
    채팅 권한 판단에 쓸 AgitRoomReference와 멤버 상태(ACTIVE/LEFT/BANNED)를 순수 도메인으로 둔다.

---
+++ src/main/java/com/plip/chat/domain/model/AgitMemberRole.java
@@ -0,0 +1,6 @@
+package com.plip.chat.domain.model;
+
+public enum AgitMemberRole {
+	HOST,
+	GUEST
+}
---
+++ src/main/java/com/plip/chat/domain/model/AgitRoomStatus.java
@@ -0,0 +1,6 @@
+package com.plip.chat.domain.model;
+
+public enum AgitRoomStatus {
+	ACTIVE,
+	DELETED
+}

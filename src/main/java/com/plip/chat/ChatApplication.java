3afce0477130ff7969943399413fca1b5291b213
headdrop <ehdgmltn@gmail.com>
2026-08-18 10:22:48 +0900

    Chore: 기본 세팅
    template을 기초로 초기 세팅 변경 적용

--- src/main/java/com/plip/chat/ChatApplication.java
+++ src/main/java/com/plip/chat/ChatApplication.java
@@ -1,13 +1,13 @@
-package com.plip.template;
+package com.plip.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class ChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(ChatApplication.class, args);
 	}
 
 }
